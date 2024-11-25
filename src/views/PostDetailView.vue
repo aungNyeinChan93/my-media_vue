5929a86374dd58ae497f431963b3ecc6466a6392
nyeinchan <aungNyeinChan.dev@gmail.com>
2024-11-25 15:51:03 +0630

    postView-actionLog

--- src/views/PostDetailView.vue
+++ src/views/PostDetailView.vue
@@ -44,39 +44,48 @@
             </div>
             <div class="col-lg-4">
               <!-- Section Tittle -->
+
               <div class="section-tittle mb-40">
-                <h3>Follow Us</h3>
+                <h3>Reaction</h3>
               </div>
+
               <!-- Flow Socail -->
               <div class="single-follow mb-45">
                 <div class="single-box">
                   <div class="follow-us d-flex align-items-center">
                     <div class="follow-social">
-                      <a href="#"><img src="assets/img/news/icon-fb.png" alt="" /></a>
+                      <!-- <a href="#"><img src="assets/img/news/icon-fb.png" alt="" /></a> -->
+                      LIKE
                     </div>
                     <div class="follow-count">
                       <span>8,045</span>
                       <p>Fans</p>
                     </div>
                   </div>
+
                   <div class="follow-us d-flex align-items-center">
                     <div class="follow-social">
-                      <a href="#"><img src="assets/img/news/icon-tw.png" alt="" /></a>
+                      <!-- <a href="#"><img src="assets/img/news/icon-tw.png" alt="" /></a> -->
+                      COMMENT
                     </div>
                     <div class="follow-count">
                       <span>8,045</span>
                       <p>Fans</p>
                     </div>
                   </div>
+
                   <div class="follow-us d-flex align-items-center">
                     <div class="follow-social">
-                      <a href="#"><img src="assets/img/news/icon-ins.png" alt="" /></a>
+                      <i class="fas fa-eye"></i>
+                      <!-- <a href="#"><img src="assets/img/news/icon-ins.png" alt="" /></a> -->
                     </div>
                     <div class="follow-count">
-                      <span>8,045</span>
-                      <p>Fans</p>
+                      <span>{{ viewCounts }}</span>
+                      <p v-if="viewCounts <= 1">view</p>
+                      <p v-else>views</p>
                     </div>
                   </div>
+
                   <div class="follow-us d-flex align-items-center">
                     <div class="follow-social">
                       <a href="#"><img src="assets/img/news/icon-yo.png" alt="" /></a>
@@ -111,25 +120,28 @@
 </template>
 
 <script>
+import store from "@/store";
 import axios from "axios";
 import { onMounted, ref } from "vue";
 import { useRoute } from "vue-router";
-import { mapGetters } from "vuex";
+import { mapGetters, mapState, useStore } from "vuex";
 
 export default {
   name: "PostDetailView",
   props: {
     id: {
-      type: Number,
+      // type: Number,
       required: true,
     },
   },
 
   setup(props) {
+    const store = useStore();
     const route = useRoute();
     const postId = route.params.id;
     const post = ref({});
     const category = ref("");
+    const viewCounts = ref("");
 
     const getpostData = async (id) => {
       const res = await axios.get(`http://localhost:8000/api/posts/detail/${id}`);
@@ -143,16 +155,37 @@ export default {
       category.value = res.data.category;
     };
 
+    const auth = store.state.userData;
+
+    const viewActionlog = async (user_id, post_id) => {
+      try {
+        const res = await axios.post("http://localhost:8000/api/actionLogs/post/view", {
+          user_id: user_id,
+          post_id: post_id,
+        });
+        // console.log(res.data);
+        viewCounts.value = res.data.viewsCounts;
+      } catch (e) {
+        console.log(e.message);
+      }
+    };
+
     onMounted(() => {
+      getpostData(postId);
+      viewActionlog(auth.id, props.id);
       // console.log(postId);
       // console.log(props.id);
-      getpostData(postId);
+      // console.log(store.getters.getUserData);
+      // console.log(store.state.userData);
+      // console.log(auth.id);
+      // console.log(props.id);
     });
 
-    return { getpostData, post, category };
+    return { getpostData, post, category, viewCounts };
   },
   computed: {
-    ...mapGetters(["getToken"]),
+    ...mapGetters(["getToken", "getUserData"]),
+    ...mapState(["userData"]),
   },
 };
 </script>
